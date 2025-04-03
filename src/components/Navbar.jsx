41421eaeb4d9b7cf9ccbf76d47ec45fa54597825
catexstacy <stacycatex@gmail.com>
2025-04-03 15:04:10 +0300

    styled the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ import searchB from '../assets/search-b.png';
 function Navbar() {
   return (
     <>
-    <section className="m-0 box-border p-4 font-serif flex justify-between items-center bg-white text-black w-[100%]"> 
+    <section className="m-0 box-border px-8 font-serif flex justify-between items-center bg-gray-300 text-black"> 
     <nav className="flex justify-between text-center px-6 py-4">
-         <img src={logoDark} alt="" className='w-[100px] h-auto' />
-         <ul className=' mb-[20px] flex-1 space-x-6 text-lg list-none text-center inline mt-[10px]'>
+         <img src={logoDark} alt="" className='w-10 h-auto' />
+         <ul className=' mb-[20px] flex space-x-6 text-lg text-center px-[20px] justify-between'>
           <li className='cursor-pointer hover:text-yellow-400'>Home</li>
           <li className='cursor-pointer hover:text-gray-400'>Movies</li>
           <li className='cursor-pointer hover:text-gray-400'>Trending</li>
@@ -24,13 +24,13 @@ function Navbar() {
           <li className='cursor-pointer hover:text-gray-400'>About</li>
          </ul>
       </nav>
-      <div className="flex justify-between px-[3rem] align-middle rounded-[50px]">
-        <input type="text" className=" bg-blue-400 rounded-lg px-4 py-2 border-0 outline-0 text-lg max-w-[20px]" placeholder='search' />
-        <img src={searchB} alt="" className="w-6 h-auto cursor-pointer" />
+      <div className="flex justify-between items-center px-[10px]">
+        <input type="text" className=" bg-blue-400 rounded-lg px-4 py-2 border-0 outline-0 text-lg" placeholder='search' />
+        <img src={searchB} alt="" className="w-10 h-auto cursor-pointer p-2" />
   
       </div>
       <div className="">
-      <img src={nightImg} alt="" className="w-6 h-auto cursor-pointer" />
+      <img src={nightImg} alt="" className="w-10 h-auto cursor-pointer" />
       </div>
     </section>
     </>
