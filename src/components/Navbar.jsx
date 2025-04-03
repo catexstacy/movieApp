0be3fb31af69a9368c0409a12e6db49aa0f3f43e
catexstacy <stacycatex@gmail.com>
2025-04-03 14:00:39 +0300

    added style to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ import searchB from '../assets/search-b.png';
 function Navbar() {
   return (
     <>
-    <section className="m-0 box-border p-4 font-serif flex justify-between items-center bg-gray-900 text-white"> 
+    <section className="m-0 box-border p-4 font-serif flex justify-between items-center bg-white text-black w-[100%]"> 
     <nav className="flex justify-between text-center px-6 py-4">
          <img src={logoDark} alt="" className='w-[100px] h-auto' />
-         <ul className=' mx-[2rem] flex space-x-6 text-lg'>
+         <ul className=' mb-[20px] flex-1 space-x-6 text-lg list-none text-center inline mt-[10px]'>
           <li className='cursor-pointer hover:text-yellow-400'>Home</li>
           <li className='cursor-pointer hover:text-gray-400'>Movies</li>
           <li className='cursor-pointer hover:text-gray-400'>Trending</li>
@@ -24,8 +24,8 @@ function Navbar() {
           <li className='cursor-pointer hover:text-gray-400'>About</li>
          </ul>
       </nav>
-      <div className="flex justify-between px-[3rem] align-middle">
-        <input type="text" className=" bg-blue-400 rounded-lg px-4 py-2" placeholder='search' />
+      <div className="flex justify-between px-[3rem] align-middle rounded-[50px]">
+        <input type="text" className=" bg-blue-400 rounded-lg px-4 py-2 border-0 outline-0 text-lg max-w-[20px]" placeholder='search' />
         <img src={searchB} alt="" className="w-6 h-auto cursor-pointer" />
   
       </div>
